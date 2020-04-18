c50442b46afecc413ccefcc443578f1ee97b0c72
dev-173 <63912836+dev-173@users.noreply.github.com>
2020-04-18 23:26:15 +0300

    Now it has the text in the top left cornor
    no stroke for text

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import wget
 from datetime import datetime
 import os
-import image_tools
 import requests
-import base64
 import sys
 
-urlphoto = 'http://192.168.1.33:8080/photoaf.jpg
-ph = requests.get(urlphoto)
-foto = ph.text
-print(sys.getsizeof(foto))
-print(ph)
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
-# x = 1
-# while True:
-#     now = datetime.now()
-#     dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
-#     filename = dt_string+'.jpg'
-#     newname = os.path.join('/Users/bluepill/Documents/proj/ipcamrea/photos' , filename)
-#     photourl = 'http://192.168.1.33:8080/photoaf.jpg'
-#     wget.download(photourl , newname)
-#     fileinfo = os.stat(newname)
-#     print(fileinfo.st_size)
+x = 1
+while True:
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
+    filename = dt_string+'.jpg'
+    newname = os.path.join('/Users/bluepill/Documents/proj/ipcamrea/photos' , filename)
+    photourl = 'http://192.168.1.25:8080/photo.jpg'
+    wget.download(photourl , newname)
+    fileinfo = os.stat(newname)
+    print("/n")
     
-#     stam = os.system('cat %newname')
-#     print(stam)
-# x += 1
 
+    im = Image.open(newname)
+    helvetica = ImageFont.truetype("/Users/bluepill/AppData/Local/Microsoft/Windows/Fonts/a_FuturaOrto-Bold.ttf", size=40)
+    d = ImageDraw.Draw(im)
 
+    location = (0, 0)
+    text_color = (255, 255, 255)
+    d.text(location, dt_string, font=helvetica, fill=text_color)
 
+    im.save("/Users/bluepill/Documents/proj/ipcamrea/photos/"+dt_string+".jpg")
+x += 1
